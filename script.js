db1310ad63bd2e53e0da0aeb89554e2e663c1d2e
Joyce Wambui <joycewambui@gmail.com>
2024-05-29 15:59:45 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById('dark-mode-toggle').addEventListener('change', function() {
     document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
 });
